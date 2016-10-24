7b86bcfba78d272475e1b8e41de4832bac2ee8a6
Dongyi Yang <dongyi.yang@vmturbo.com>
2016-10-24 15:24:04 -0400

    Added Default Port 2222

--- cmd/app/options/options.go
+++ cmd/app/options/options.go
@@ -23,5 +23,5 @@ func (s *K8sConntrackConfig) AddFlags(fs *pflag.FlagSet) {
 	fs.StringVar(&s.Master, "master", s.Master, "The address of the Kubernetes API server (overrides any value in kubeconfig)")
 	fs.StringVar(&s.Kubeconfig, "kubeconfig", s.Kubeconfig, "Path to kubeconfig file with authorization and master location information.")
 	fs.StringVar(&s.ConntrackBindAddress, "conntrack-bind-address", s.ConntrackBindAddress, "The IP address for the conntrack server to serve on, defaulting to 0.0.0.0 (set to 127.0.0.1 for local).")
-	fs.StringVar(&s.ConntrackPort, "conntrack-port", s.ConntrackPort, "The port to bind the k8sconntrack server.")
+	fs.StringVar(&s.ConntrackPort, "conntrack-port", "2222", "The port to bind the k8sconntrack server.")
 }
